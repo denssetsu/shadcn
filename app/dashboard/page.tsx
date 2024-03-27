87df89b42ea77aa523129583cb21f5a444665396
turki alsubaie <troki0677@gmail.com>
2024-03-27 23:25:09 +0300

    employee dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EmployeeStats from "./components/Employees-stats";
+
 export default function DashboardPage() {
-  return <div>dashboard page</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmployeeStats />
+      </TabsContent>
+      <TabsContent value="teams">Teams stats view</TabsContent>
+    </Tabs>
+  );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -23,6 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
+        xs: "text-xs  px-3 py-2 tracking-normal",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
@@ -47,6 +48,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={twMerge(
           "tracking-wider",
+
           "font-bold",
           "uppercase",
           buttonVariants({ variant, size, className })
